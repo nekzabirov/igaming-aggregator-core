617bcfc2dda48ac9f80243723fae8a8f5455a7b0
nekzabirov <n.zabirov@1638.tech>
2025-12-01 19:46:01 +0200

    Fix koin in route

--- src/main/kotlin/infrastructure/aggregator/onegamehub/handler/OneGameHubRoute.kt
+++ src/main/kotlin/infrastructure/aggregator/onegamehub/handler/OneGameHubRoute.kt
@@ -5,7 +5,7 @@ import io.ktor.http.Parameters
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
-import org.koin.ktor.ext.getKoin
+import org.koin.ktor.ext.inject
 import shared.value.SessionToken
 
 private val Parameters.amount get() = this["amount"]!!.toInt()
@@ -15,23 +15,25 @@ private val Parameters.roundId get() = this["round_id"]!!
 private val Parameters.freespinId get() = this["freerounds_id"]
 private val Parameters.isRoundEnd get() = this["ext_round_finished"] == "1"
 
-internal fun Route.oneGameHubWebhookRoute() = post("/onegamehub") {
-    val handler = getKoin().get<OneGameHubHandler>()
+internal fun Route.oneGameHubWebhookRoute() {
+    val handler by inject<OneGameHubHandler>()
 
-    val action = call.queryParameters["action"]
-    val sessionToken = call.queryParameters["extra"]
+    post("/onegamehub") {
+        val action = call.queryParameters["action"]
+        val sessionToken = call.queryParameters["extra"]
 
-    if (action == null || sessionToken == null) {
-        call.respond(OneGameHubResponse.Error.OneGameHubInvalidateRequest)
-        return@post
+        if (action == null || sessionToken == null) {
+            call.respond(OneGameHubResponse.Error.OneGameHubInvalidateRequest)
+            return@post
+        }
+
+        val mainToken = SessionToken(sessionToken)
+
+        val response = when (action) {
+            "balance" -> handler.balance(mainToken)
+            else -> OneGameHubResponse.Error.OneGameHubInvalidateRequest
+        }
+
+        call.respond(response)
     }
-
-    val mainToken = SessionToken(sessionToken)
-
-    val response = when (action) {
-        "balance" -> handler.balance(mainToken)
-        else -> OneGameHubResponse.Error.OneGameHubInvalidateRequest
-    }
-
-    call.respond(response)
 }
